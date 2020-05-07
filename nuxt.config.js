910faefbeadf469a1765f1f6ba4fcafeadd8400a
mal <themarkymark@protonmail.com>
2020-05-07 09:34:01 +0200

    Update nuxt base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/hive-hackathon-www/'
+    base: '/'
   }
 } : {}
 
